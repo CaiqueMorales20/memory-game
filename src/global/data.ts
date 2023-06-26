fdff9aaaf12ff6e6036f6b9754280b98f776ade3
Caique <caiquemorales20@gmail.com>
2023-06-26 19:53:55 -0300

    :sparkles: feat: Global Data

---
+++ src/global/data.ts
@@ -0,0 +1,21 @@
+// Images
+import DogImg from "../assets/img/CACHORRO.jpg";
+import KoalaImg from "../assets/img/CACHORRO.jpg";
+import FlamingoImg from "../assets/img/CACHORRO.jpg";
+import KittenImg from "../assets/img/CACHORRO.jpg";
+import AlligatorImg from "../assets/img/CACHORRO.jpg";
+import ChickImg from "../assets/img/CACHORRO.jpg";
+import PigImg from "../assets/img/CACHORRO.jpg";
+import CowImg from "../assets/img/CACHORRO.jpg";
+
+// Data
+export const Data = [
+	{ id: 1, name: "dog", img: DogImg },
+	{ id: 2, name: "koala", img: KoalaImg },
+	{ id: 3, name: "flamingo", img: FlamingoImg },
+	{ id: 4, name: "kitten", img: KittenImg },
+	{ id: 5, name: "alligator", img: AlligatorImg },
+	{ id: 6, name: "chick", img: ChickImg },
+	{ id: 7, name: "pig", img: PigImg },
+	{ id: 8, name: "cow", img: CowImg },
+];
