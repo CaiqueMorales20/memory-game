cf35995f14477da015f8e44e66af22e010281906
Caique <caiquemorales20@gmail.com>
2023-06-25 21:01:32 -0300

    :tada: First Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// Functional Component
+const App = () => {
+	// Rendering
+	return <>Memory</>;
+};
+
+// Export
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+// Imports
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// App
+import App from './App.tsx'
+
+// Rendering
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
